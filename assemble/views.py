3f33666f5df561e216cff933b6f001302b7a5de8
Kristian <kristian.roopnarine@gmail.com>
2020-03-14 08:41:23 -0400

    Added modal form to create component tasks.

--- assemble/views.py
+++ assemble/views.py
@@ -83,7 +83,7 @@ class ProjectTaskCreate(LoginRequiredMixin,CreateView):
         form.instance.task = component
         super().form_valid(form)
         messages.success(self.request,f"Added {form.instance.name} to {form.instance.task}")
-        return redirect('create-task',project_component_slug=component.slug)
+        return redirect(component.project)
     
     def get_context_data(self,**kwargs):
         context= super().get_context_data(**kwargs)
