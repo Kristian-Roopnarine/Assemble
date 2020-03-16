87d4482720c237ad1843e6329c5c22f3b3ec7089
Kristian <kristian.roopnarine@gmail.com>
2020-03-16 00:25:05 -0400

    Added documention and organized urls.py file to be clear about where they're directing the user to.

--- assemble/urls.py
+++ assemble/urls.py
@@ -29,40 +29,53 @@ from .views import (
 
 urlpatterns = [
     path('',index,name='home'),
+    
+    ############################################
+    ### USER AUTH VIEWS
+    ############################################
     path('accounts/sign-up',sign_up,name='sign-up'),
     path('logout',log_out,name='logged_out'),
+    
+
+    ############################################
+    ### PROJECT URLS
+    ############################################
     path('project-list/',ProjectList.as_view(),name="project-list"),
     path('project-create/',ProjectCreate.as_view(),name="project-create"),
-
-    # displays the current project and all project components
     path('project-list/project-detail/<project_slug>/',project_detail_view,name="project-detail"),
+    path('project-list/delete-project/<pk>/',delete_project,name='delete-project'),
+    path('project-list/edit-project/<pk>/',ProjectEditView.as_view(),name='edit-project'),
 
     # displays the current project component and all project component tasks
     #path('project-component-detail/<project_component_slug>/',project_component_detail_view,name="project-component-detail"),
 
-    #create components for a specific project
+    ############################################
+    ### PROJECT COMPONENT/TASK URLS
+    ############################################
     path('project-list/project-detail/project-component-create/<project_slug>/',ProjectComponentCreate.as_view(),name="project-component-create"),
-    # displays one task
-    #path('project-component-detail/<project_component_slug>/<component_task_slug>/',component_task_detail,name="component-task-detail"),
-    
-    # create tasks for a component
     path('project-list/project-detail/component-task-create/<project_component_slug>/',ProjectTaskCreate.as_view(),name='create-task'),
-
-    #updates task completed
+    path('project-list/project-detail/edit-details/<pk>/',edit_component_or_task,name="edit-details"),
     path('finish-task/<pk>/',finish_task_detail,name="finish-task"),
     path('delete-task/<pk>/',delete_task,name="delete-task"),
+
+    #path('project-component-detail/<project_component_slug>/<component_task_slug>/',component_task_detail,name="component-task-detail"),
+    
+
+    ############################################
+    ### USER PROFILE AND INTERACTION URLS
+    ############################################
     path('profile/',profile,name='profile'),
     path('search/',search_user,name="search-user"),
+    path('search/profile/<slug>/',profile_view,name="profile-view"),
 
     path('search/send-friend-request/<sent_to>/',send_friend_request,name="send-friend-request"),
     path('profile/accept-friend-request/<from_user>/',accept_friend_request,name='accept-friend-request'),
     path('profile/delete-friend-request/<pk>/',delete_friend_request,name="delete-friend-request"),
 
-    path('search/profile/<slug>/',profile_view,name="profile-view"),
+    
 
-    path('project-list/delete-project/<pk>/',delete_project,name='delete-project'),
-    path('project-list/edit-project/<pk>/',ProjectEditView.as_view(),name='edit-project'),
-    path('project-list/project-detail/edit-details/<pk>/',edit_component_or_task,name="edit-details")
+    
+    
        
 ]
 
