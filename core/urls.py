544a0fd8320d856f4b85e547a1ac42a544ddba2f
Kristian <kristian.roopnarine@gmail.com>
2020-03-27 21:40:34 -0400

    Changed home URL.

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('assemble.urls')),
+    path('',include('assemble.urls')),
     path('accounts/',include('django.contrib.auth.urls'))
 ]
